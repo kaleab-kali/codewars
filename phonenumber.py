bd6ea602b1dd708ccf7b914c0089215e801b4c0c
kaliktg <86680782+kaliktg@users.noreply.github.com>
2022-10-24 12:41:34 +0000

    edited phone number kata

--- phonenumber.py
+++ phonenumber.py
@@ -4,6 +4,7 @@
     Example
     create_phone_number([1, 2, 3, 4, 5, 6, 7, 8, 9, 0]) # => returns "(123) 456-7890" 
     The returned format must be correct in order to complete this challenge.
+    
 """
 def create_phone_number(n):
         #your code here\
